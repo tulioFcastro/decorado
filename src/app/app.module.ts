c9c9a363212220287b4bacdff32135664ad98309
Tulio Castro <tulio@dynavideo.com.br>
2018-05-11 16:26:33 -0300

    Added login component; Added config to material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatTableModule,
+  MatPaginatorModule,
+  MatTabsModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatTabsModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
