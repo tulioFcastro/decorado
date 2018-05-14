06c0bd787f7f393ee36d035e4a7e18919f2e339e
Tulio Castro <tulio@dynavideo.com.br>
2018-05-14 13:15:22 -0300

    Changed attribute 'type' to 'profile'

--- src/app/in-memory-user.service.ts
+++ src/app/in-memory-user.service.ts
@@ -3,8 +3,8 @@ import { User } from './User';
 export class InMemoryUserService {
   createDb() {
     const users: User[] = [
-      { id: 1, login: 'admin', password: 'admin', type: 'admin' },
-      { id: 2, login: 'user', password: 'user', type: 'default' }
+      { id: 1, login: 'admin', password: 'admin', profile: 'ADMIN' },
+      { id: 2, login: 'user', password: 'user', profile: 'USER' }
     ];
     return { users };
   }
